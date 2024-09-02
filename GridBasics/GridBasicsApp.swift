4e00029bec8b19687624b25d62faf4719e0dd105
Nour <mohamednour25@gmail.com>
2024-09-02 22:12:30 +0300

    Initial Commit

---
+++ GridBasics/GridBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridBasicsApp.swift
+//  GridBasics
+//
+//  Created by administrator on 02/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct GridBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
